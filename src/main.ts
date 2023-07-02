3faf43b0983492d206a2c33583ef32d7511f71b1
andrevitor103 <andrevitor103@gmail.com>
2023-07-02 18:17:21 -0300

    criando tabela

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,31 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import PrimeVue from 'primevue/config';
+
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primevue/resources/primevue.min.css";
+
+import Button from "primevue/button"
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
+const app = createApp(App);
+
+app.component('Button', Button);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+
 
 app.use(router)
+
+app.use(PrimeVue)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main>   
+    <div class="card">
+        <DataTable :value="customers" tableStyle="min-width: 50rem" editMode="row" dataKey="email" tableClass="editable-cells-table">
+            <Column field="name" header="nome"></Column>
+            <Column field="email" header="email"></Column>
+            <Column field="document" header="documento"></Column>
+            <Column  header="" :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
+        </DataTable>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      customers: [
+        {
+          'name': 'André Vitor',
+          'email': 'andrevitor103@gmail.com',
+          'document': '999.999.99-99',
+        },
+      ]
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
