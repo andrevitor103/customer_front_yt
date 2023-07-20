ca078afc263bce5534d1b1649b1bdaec98c3aadb
andrevitor103 <andrevitor103@gmail.com>
2023-07-20 20:04:10 -0300

    criando componente para linhas da tabela

---
+++ src/components/RecordTable.vue
@@ -0,0 +1,25 @@
+<template>
+     <td>{{ customer.name }}</td>
+        <td>{{ customer.email }}</td>
+        <td>{{ customer.document }}</td>
+        <td>
+          <v-btn color="black" @click="edit(index)">
+            <v-icon color="white darken-2" class="edit-btn">edit</v-icon>
+          </v-btn>
+        </td> 
+</template>
+
+<script lang="js">
+    export default {
+        name: 'RecordTable',
+        data() {
+            return {
+                customer: {
+                    'name': 'andrevitor103',
+                    'email': 'andrevitor103@gmail.com',
+                    'document': '9999999999'
+                }
+            }
+        }
+    }
+</script>
